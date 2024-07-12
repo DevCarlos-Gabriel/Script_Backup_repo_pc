e84e015e360281c819c4668928f3b696880c15a1
Carlos Gabriel <carlosgabriel03082005@gmail.com>
2024-07-12 10:58:02 -0300

    Add Script

---
+++ backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+GITHUB_TOKEN="user_token"
+
+BACKUP_DIR="Backups_repo_pc"
+
+BACKUP_DIR_REPO="Backups_repo_pc/repo"
+
+BACKUP_DIR_PC="Backups_repo_pc/pc"
+
+
+mkdir -p ~/$BACKUP_DIR
+
+mkdir -p ~/$BACKUP_DIR_REPO
+
+mkdir -p ~/$BACKUP_DIR_PC
+
+
+cd ~/$BACKUP_DIR_REPO
+
+
+REPOS=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/user/repos?per_page=100" | jq -r '.[].ssh_url')
+
+for REPO in $REPOS; do
+
+ git clone $REPO
+
+done
+
+
+cd ..
+
+cd ~/$BACKUP_DIR_PC
+
+HOME_PATH="/home/*/"
+
+rsync -av --exclude='.*' --exclude='*.log' --exclude='*.tmp' --exclude='Modelos/' \
+          --exclude='Sync/' --exclude='Público/' --exclude='Música/' --exclude='Área de Trabalho/' --exclude='Backups_repo_pc/' $HOME_PATH ~/$BACKUP_DIR_PC
+
+echo "
+
+
+
+
+
+"
+
+echo "Seus arquivos foram salvos no diretório ~/Backups_repo_pc"
+
+# Beta 0.0.1
